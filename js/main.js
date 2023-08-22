bea791e5dd09dbb3f612f28eccd0875d4509e259
gokepelemo <goke.pelemo@gmail.com>
2023-08-22 16:02:01 -0500

    added new functions for gameplay

--- js/main.js
+++ js/main.js
@@ -13,8 +13,9 @@ let homePitStones,
   selectedPit,
   winner,
   gamePlay,
+  extraTurn,
   // todo: add validation to prevent numbers outside the range of 3 and 10
-  difficulty = 3;
+  difficulty = 10;
 
 /****** cached DOM elements ******/
 const infoPane = document.querySelector("#info-pane");
@@ -28,11 +29,15 @@ class GameScene {
       name: "Player 1",
       homePit: 0,
       homePitPosition: 7,
+      pitSelected: document.querySelector("#pit-info-1"),
+      stonesSelected: document.querySelector("#stone-count-1"),
     }),
       (this.player2 = {
         name: "Player 2",
         homePit: 0,
         homePitPosition: 14,
+        pitSelected: document.querySelector("#pit-info-2"),
+        stonesSelected: document.querySelector("#stone-count-2"),
       });
     // utility function: if we get an opposing side parameter, we return the
     // opposing side. if not, we return the player who owns the position
@@ -47,22 +52,22 @@ class GameScene {
 }
 
 /****** functions ******/
-const setPlayerParams = (player) => {
+const setPlayerParams = () => {
   // set the number of stones in the homepit after each turn
-  player === "Player 1"
-    ? (gamePlay[player].homePit = document.getElementById("7").dataset.stones)
-    : (gamePlay[player].homePit = document.getElementById("14").dataset.stones);
+  gamePlay[turn].homePit = homePitStones[7];
+  gamePlay[turn].homePit = homePitStones[14];
 };
 
-// on initialization, prevent stones from being added to the home pits
+// on initialization, prevent stones from being added to the home pits.
 // on every other turn prevent stones from being added to the home pit
-// of the current player and the pit they selected for the turn
+// of the current player and the pit they selected for the turn.
 const pitTainted = (position, init) => {
+  let oppositeTurn = turn === "player1" ? "player2" : "player1";
   if (init && (position === 7 || position === 14)) return true;
-  if (position === gamePlay[turn].homePitPosition) return true;
+  if (position === gamePlay[oppositeTurn].homePitPosition) return true;
   return selectedPit ? position === selectedPit : false;
 };
-// add stone to the proper pits on game initialization
+// add stones to the proper pits on game initialization
 const initialPitStones = () => {
   for (let i = 1; i <= 14; i++) {
     // taint pit for the current player
@@ -74,9 +79,83 @@ const initialPitStones = () => {
   }
 };
 
+// play a turn
+const playTurn = (position) => {
+  // collect stones from a selected pit and distribute to all pits except the tainted pits, return the id of the last pit it drops on
+  let dropPosition,
+    stones = boardStones.collect(position);
+  position === 14 ? (dropPosition = 1) : (dropPosition = position + 1);
+  selectedPit = position;
+  while (stones > 0) {
+    if (pitTainted(dropPosition)) {
+      console.log(`${dropPosition} is tainted`);
+      dropPosition === 14 ? (dropPosition = 1) : dropPosition++;
+      continue;
+    }
+    if (dropPosition === 7 || dropPosition === 14) {
+      homePitStones.add(dropPosition);
+      stones--;
+    } else {
+      boardStones.add(dropPosition);
+      stones--;
+    }
+    dropPosition === 14 ? (dropPosition = 1) : dropPosition++;
+  }
+  setPlayerParams(turn);
+  return checkState(dropPosition);
+};
+
+const checkState = (position) => {
+  let turnState = position - 1;
+  // If the last pit is a home pit, current player plays again.
+  if (turnState === 7 || turnState === 14) {
+    extraTurn = true;
+  // If the last pit is empty and on the current player's side, all the opponent's stones on the opposite pit are added to the current player's home pit.
+  } else if (boardStones[turnState] === 1) {
+    let capturedStones = boardStones.collect(
+      gamePlay.boardPosition(turnState, true)
+    );
+    for (let i = 0; i < capturedStones; i++) {
+      homePitStones.add(gamePlay[turn].homePitPosition);
+    }
+  }
+  // Return the last position that was played.
+  return turnState
+};
 // create a message for the info pane
 const createMessage = (msg, position) => {
   infoPane.innerHTML = msg;
+};
+
+// toggle pit selection
+const togglePit = (position) => {
+  let hasBorder = document.getElementById(position).style.border != "";
+  hasBorder
+    ? (document.getElementById(position).style.border = "")
+    : (document.getElementById(position).style.border = "var(--selected-pit)");
+};
+
+// switch turns unless they landed on a home pit on their last turn
+const switchTurn = () => {
+  if (extraTurn === true) {
+    createMessage(`Landed on your home pit. You get an extra turn.`);
+    extraTurn = false;
+    return turn;
+  } else {
+    turn === "player1" ? (turn = "player2") : (turn = "player1");
+    return turn;
+  }
+};
+
+// coin toss
+const coinToss = () => {
+  return Math.ceil(Math.random() * 2) === 1 ? "Heads" : "Tails";
+};
+
+// handle player selecting a pit
+const pitSelect = (position) => {
+  gamePlay[turn].pitSelected.innerHTML = `Pit ${position} Selected`;
+  gamePlay[turn].stonesSelected.innerHTML = `${boardStones[position]}`;
 };
 
 // add a stone to a pit on the game board
@@ -110,7 +189,7 @@ const createPits = () => {
     pitPosition--;
   }
 };
-// whenever the pit stone objects are updated, also update the DOM
+// whenever the pit stone objects are updated, also update the DOM.
 const updatePit = (position) => {
   let numPosition = position.toString();
   if (position === 7 || position === 14) {
